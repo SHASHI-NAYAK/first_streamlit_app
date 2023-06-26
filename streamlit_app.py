2971e5b263c672f3cbfddacfb60fc0725b29281c
SHASHI-NAYAK <137814967+SHASHI-NAYAK@users.noreply.github.com>
2023-06-26 18:40:13 +0100

    Update streamlit_app.py
    change function font 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents Healthy New Diner')
-streamlit.Header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Freee-Range Egg')
