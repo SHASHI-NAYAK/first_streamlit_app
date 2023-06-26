53879db424d81e21b384358116dec634f9980989
SHASHI-NAYAK <137814967+SHASHI-NAYAK@users.noreply.github.com>
2023-06-26 18:39:41 +0100

    Update streamlit_app.py
    revert function font

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My Parents Healthy New Diner')
 streamlit.Header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.TEXT('Kale Spinach & Rocket Smoothie')
-streamlit.Text('Hard-Boiled Freee-Range Egg')
+streamlit.text('Kale Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Freee-Range Egg')
