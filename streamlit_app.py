ee44cce017e61bedc5606dd30a1ad8f8ac2bcc84
SHASHI-NAYAK <137814967+SHASHI-NAYAK@users.noreply.github.com>
2023-06-26 18:38:19 +0100

    Update streamlit_app.py
    add header and change function font.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents Healthy New Diner')
-streamlit.write('Omega 3 & Blueberry Oatmeal')
-streamlit.write('Kale Spinach & Rocket Smoothie')
-streamlit.write('Hard-Boiled Freee-Range Egg')
+streamlit.Header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.TEXT('Kale Spinach & Rocket Smoothie')
+streamlit.Text('Hard-Boiled Freee-Range Egg')
