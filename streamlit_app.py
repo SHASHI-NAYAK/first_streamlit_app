3216e5486d8b000224a9aa6cb8cd73f51385a783
SHASHI-NAYAK <137814967+SHASHI-NAYAK@users.noreply.github.com>
2023-06-26 18:36:32 +0100

    Update streamlit_app.py
    Add 2 more write comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 
 streamlit.title('My Parents Healthy New Diner')
 streamlit.write('Omega 3 & Blueberry Oatmeal')
+streamlit.write('Kale Spinach & Rocket Smoothie')
+streamlit.write('Hard-Boiled Freee-Range Egg')
